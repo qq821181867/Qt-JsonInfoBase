8552f77e0798e2f36991e666aa99fd37f0c1d9c3
yisen.Li <821181867@qq.com>
2021-03-10 01:15:46 +0800

    Add files via upload
    添加JsonInfoBase类相关文件

---
+++ JsonInfoBase.cpp
@@ -0,0 +1,105 @@
+﻿#include "JsonInfoBase.h"
+#include <QDebug>
+#include <QMetaProperty>
+bool JsonInfoBase::fromJson(const QJsonObject &obj)
+{
+    QList<QByteArray> listPropertyNames = getPropertyNames();
+
+    foreach (const QByteArray &name, listPropertyNames)
+    {
+        if(!obj.contains(name))
+        {
+            qDebug()<<__FUNCTION__<<"error:"<<name;
+            continue;
+        }
+        setProperty(name,obj.value(name));
+    }
+
+    return true;
+}
+
+QJsonObject JsonInfoBase::toJson() const
+{
+    QJsonObject obj;
+    //
+    QList<QByteArray> listPropertyNames = getPropertyNames();
+    //
+    foreach (const QByteArray &name, listPropertyNames)
+    {
+        if(!property(name).isValid() || property(name).isNull())
+        {
+            continue;
+        }
+        obj.insert(name,propertyDataToJsonValue(property(name)));
+    }
+    //
+    return obj;
+}
+
+QJsonObject JsonInfoBase::toJsonInSections(const QList<QByteArray> &sections) const
+{
+    QJsonObject obj;
+    //
+    foreach (const QByteArray &name, sections)
+    {
+        if(!property(name).isValid() || property(name).isNull())
+        {
+            continue;
+        }
+        obj.insert(name,propertyDataToJsonValue(property(name)));
+    }
+    //
+    return obj;
+}
+
+QJsonObject JsonInfoBase::toJsonOutSections(const QList<QByteArray> &sections) const
+{
+    QJsonObject obj;
+    //
+    QList<QByteArray> listPropertyNames = getPropertyNames();
+    foreach (auto section, sections)
+    {
+        listPropertyNames.removeAll(section);
+    }
+    //
+    foreach (const QByteArray &name, listPropertyNames)
+    {
+        if(!property(name).isValid() || property(name).isNull())
+        {
+            continue;
+        }
+        obj.insert(name,propertyDataToJsonValue(property(name)));
+    }
+    //
+//    qDebug()<<__FUNCTION__<<obj;
+    return obj;
+}
+
+QList<QByteArray> JsonInfoBase::getPropertyNames() const
+{
+    QList<QByteArray> listPropertyNames;
+    auto windowMetaObject = this->metaObject();
+    for (int i=0; i < windowMetaObject->propertyCount(); i++)
+    {
+        listPropertyNames<<windowMetaObject->property(i).name();
+    }
+    listPropertyNames.removeAll("objectName");//移除自带的
+    return listPropertyNames;
+}
+
+QJsonValue JsonInfoBase::propertyDataToJsonValue(const QVariant &data) const
+{
+    if(data.type()==QMetaType::QJsonObject)
+    {
+        return data.toJsonObject();
+    }
+    else if(data.type()==QMetaType::QJsonValue)
+    {
+        return data.toJsonValue();
+    }
+    else if(data.type()==QMetaType::QJsonArray)
+    {
+        return data.toJsonArray();
+    }
+    return QJsonValue::fromVariant(data);
+}
---
+++ JsonInfoBase.h
@@ -0,0 +1,26 @@
+﻿#ifndef JSONINFOBASE_H
+#define JSONINFOBASE_H
+
+#include <QJsonObject>
+#include <QObject>
+#include <QPointer>
+#include <QJsonArray>
+
+#define ADDMEMBER(TYPE,NAME,DEFULT) Q_PROPERTY(TYPE NAME MEMBER m_##NAME) \
+                             TYPE m_##NAME=DEFULT
+
+class JsonInfoBase : public QObject
+{
+    Q_OBJECT
+public:
+    bool fromJson(const QJsonObject &obj);
+    QJsonObject toJson() const;
+    QJsonObject toJsonInSections(const QList<QByteArray> &sections) const;  //只转传入的部分变json
+    QJsonObject toJsonOutSections(const QList<QByteArray> &sections) const; //传入的字段是不转json
+
+protected:
+    QList<QByteArray> getPropertyNames() const;
+    QJsonValue propertyDataToJsonValue(const QVariant &data) const;
+};
+
+#endif // JSONINFOBASE_H
